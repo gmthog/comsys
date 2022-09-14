7d32f0570d94834102f3010369628205d2b2d288
gmthog <101091636+gmthog@users.noreply.github.com>
2022-09-14 17:06:50 +0700

    Update doorbell_sourcdecode.py

--- doorbell_sourcdecode.py
+++ doorbell_sourcdecode.py
@@ -1,13 +1,12 @@
+
 DOORBELL_PIN = 26
 DOORBELL_SCREEN_ACTIVE_S = 60
-# Path to the SFX file
-RING_SFX_PATH = None  # If None, no sound effect plays
-# RING_SFX_PATH = "/home/pi/ring.wav"
-SENDER_EMAIL = 'senderr999@gmail.com'
-SENDER_PASSWORD = 'qlehnifbbnixrivf'
-SENDER_EMAIL = 'phamminhhoang317@gmail.com'
-RING_SFX_PATH = Doorbell-SoundBible.com-516741062.mp3
-
+JITSI_ID = None  # If None, the program generates a random UUID
+# RING_SFX_PATH = None 
+ENABLE_EMAIL = True
+FROM_EMAIL = 'senderr999@gmail.com'
+FROM_EMAIL_PASSWORD = 'hiyjyuzpjcymuiaa'
+TO_EMAIL = 'phamminhhoang317@gmail.com'
 
 import time
 import os
@@ -15,59 +14,61 @@ import signal
 import subprocess
 import smtplib
 import uuid
+
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.MIMEImage import MIMEImage
-import RPi.GPIO as GPIO
 
-# try:
-# except RuntimeError:
-#     print("Error importing RPi.GPIO. This is probably because you need superuser. Try running again with 'sudo'.")
-
-
-# def show_screen():
-#     os.system("tvservice -p")
-#     os.system("xset dpms force on")
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO.")
 
 
-# def hide_screen():
-#     os.system("tvservice -o")
+def show_screen():
+    os.system("tvservice -p")
+    os.system("xset dpms force on")
+
+
+def hide_screen():
+    os.system("tvservice -o")
 
 
 def send_email_notification(chat_url):
-    sender = EmailSender(SENDER_EMAIL, SENDER_PASSWORD)
-    email = Email(
-        sender,
-        'Video Doorbell',
-        'Notification: A visitor is waiting',
-        'A video doorbell caller is waiting on the virtual meeting room. Meet them at %s' % chat_url
-    )
-    email.send(SENDER_EMAIL)
+    if ENABLE_EMAIL:
+        sender = EmailSender(FROM_EMAIL, FROM_EMAIL_PASSWORD)
+        email = Email(
+            sender,
+            'Video Doorbell',
+            'Notification: A visitor is waiting',
+            'A video doorbell caller is waiting on the virtual meeting room. Meet them at %s' % chat_url
+        )
+        email.send(TO_EMAIL)
 
 
 def ring_doorbell(pin):
-    SoundEffect(RING_SFX_PATH).play()
+#     SoundEffect(RING_SFX_PATH).play()
 
-    chat_id = str(uuid.uuid4())
+    chat_id = JITSI_ID if JITSI_ID else str(uuid.uuid4())
     video_chat = VideoChat(chat_id)
     send_email_notification(video_chat.get_chat_url())
    
-    # show_screen()
+    show_screen()
 
     video_chat.start()
     time.sleep(DOORBELL_SCREEN_ACTIVE_S)
     video_chat.end()
 
-    # hide_screen()
+    hide_screen()
 
 
-class SoundEffect:
-    def __init__(self, filepath):
-        self.filepath = filepath
+# class SoundEffect:
+#     def __init__(self, filepath):
+#         self.filepath = filepath
 
-    def play(self):
-        if self.filepath:
-            subprocess.Popen(["aplay", self.filepath])
+#     def play(self):
+#         if self.filepath:
+#             subprocess.Popen(["aplay", self.filepath])
 
 
 class VideoChat:
@@ -82,7 +83,7 @@ class VideoChat:
         if not self._process and self.chat_id:
             self._process = subprocess.Popen(["chromium-browser", "-kiosk", self.get_chat_url()])
         else:
-            print("Can't start video chat")
+            print("Can't start video chat -- already started or missing chat id")
 
     def end(self):
         if self._process:
@@ -102,11 +103,11 @@ class Email:
         self.preamble = preamble
         self.body = body
 
-    def send(self, sender_email):
+    def send(self, to_email):
         msgRoot = MIMEMultipart('related')
         msgRoot['Subject'] = self.subject
         msgRoot['From'] = self.sender.email
-        msgRoot['To'] = sender_email
+        msgRoot['To'] = to_email
         msgRoot.preamble = self.preamble
 
         msgAlternative = MIMEMultipart('alternative')
@@ -117,7 +118,7 @@ class Email:
         smtp = smtplib.SMTP('smtp.gmail.com', 587)
         smtp.starttls()
         smtp.login(self.sender.email, self.sender.password)
-        smtp.sendmail(self.sender.email, sender_email, msgRoot.as_string())
+        smtp.sendmail(self.sender.email, to_email, msgRoot.as_string())
         smtp.quit()
 
 
@@ -128,7 +129,7 @@ class Doorbell:
     def run(self):
         try:
             print("Starting Doorbell...")
-            # hide_screen()
+            hide_screen()
             self._setup_gpio()
             print("Waiting for doorbell rings...")
             self._wait_forever()
